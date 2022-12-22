310e58d77c5d08940ca19926abb8a080db3f9362
DataSwell <63445819+DataSwell@users.noreply.github.com>
2022-12-22 15:17:55 -0500

    Initial commit

---
+++ code/insert_data_bulk.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import requests
+
+'''
+Updates: 
+- 2021-02-04: Created the TestSample.csv file in the new data folder and added the path to it here . This way you don't have to create it yourself
+Notes:
+- If you run into a json-body not found error, than you forgot to configure the application/json mapping template in the method
+- Some students had the problem: They get 200 here but the ClodWatch log of the Lambda says KeyError: 'context'  -- I can only force this when I dont't send in a payload to the API. Make sure you send data
+- If you get 403 error: Make sure to add the resource name you created in API gateway to the URL . In my case "hello". If you just copy out your URL from the "stage" in the UI then this is missing. 
+'''
+
+# URL of your endpoint
+URL = "https://ia4n6jlhbf.execute-api.us-east-1.amazonaws.com/ecommerce_prod/"
+
+
+#read the testfile
+data = pd.read_csv(r'D:\Kurse-Tutorials\AWS\data\testdata.csv', sep = ',')
+print(data)
+print('Count dataframe rows:' + str(len(data)))
+
+
+# cleaning the dataset and remove rows where one or more of the values are missing: invoiceno, stockcode, quantity, customerid
+data = data.dropna(subset=['InvoiceNo', 'StockCode', 'Quantity', 'CustomerID'])
+print('Count dataframe rows:' + str(len(data)))
+
+
+# write all the rows from the ecommerce data to the api as put request
+for i in data.index:
+    try:
+        # convert the row to json
+        export = data.loc[i].to_json()
+
+        #send it to the api
+        response = requests.post(URL, data = export)
+
+        # print the returncode
+        print(export)
+        print(response)
+    except:
+        print(data.loc[i])
