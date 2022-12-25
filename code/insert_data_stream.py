b346141412d1fca116e966497b7728c1756fc49e
DataSwell <63445819+DataSwell@users.noreply.github.com>
2022-12-25 09:08:23 -0500

    Update insert_data_stream.py

--- code/insert_data_stream.py
+++ code/insert_data_stream.py
@@ -1,15 +1,8 @@
+import random
 import pandas as pd
 import requests
-import numpy as np
+import time
 
-'''
-Updates: 
-- 2021-02-04: Created the TestSample.csv file in the new data folder and added the path to it here . This way you don't have to create it yourself
-Notes:
-- If you run into a json-body not found error, than you forgot to configure the application/json mapping template in the method
-- Some students had the problem: They get 200 here but the ClodWatch log of the Lambda says KeyError: 'context'  -- I can only force this when I dont't send in a payload to the API. Make sure you send data
-- If you get 403 error: Make sure to add the resource name you created in API gateway to the URL . In my case "hello". If you just copy out your URL from the "stage" in the UI then this is missing. 
-'''
 
 # URL of your endpoint
 URL = "https://ia4n6jlhbf.execute-api.us-east-1.amazonaws.com/ecommerce_prod/"
@@ -24,28 +17,24 @@ print('Count dataframe rows:' + str(len(data)))
 # cleaning the dataset and remove rows where one or more of the values are missing: invoiceno, stockcode, quantity, customerid
 data_new = data.dropna(subset=['InvoiceNo', 'StockCode', 'Quantity', 'CustomerID'])
 print(data_new)
-print('Count dataframe rows:' + str(len(data)))
+print('Count dataframe rows:' + str(len(data_new)))
 
 
-# splitting the dataframe in multiple
-
-
-
-# simulate streaming = random amount between 100 and 500 rows sended in a random time between 3 and 20 secondes
-
-
-
+# simulate streaming, sleep random amount between 0,2 and 3 seconds per row
 # write all the rows from the subset to the api as put request
-# for i in data.index:
-#     try:
-#         # convert the row to json
-#         export = data.loc[i].to_json()
+for i in data_new.index:
+    try:
+        # convert the row to json
+        export = data_new.loc[i].to_json()
 
-#         #send it to the api
-#         response = requests.post(URL, data = export)
+        #send it to the api
+        response = requests.post(URL, data = export)
 
-#         # print the returncode
-#         print(export)
-#         print(response)
-#     except:
-#         print(data.loc[i])
+        # print the returncode
+        print(f'{response} - {export}') 
+        sleeptime = random.uniform(0.2, 3)
+        print(f'Sleep: {sleeptime}')
+        time.sleep(sleeptime)
+
+    except:
+        print(data_new.loc[i])
